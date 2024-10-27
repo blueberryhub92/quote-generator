f91e62fce150f23eecd0a8874e195287e9a60bae
blueberryhub92 <56682282+blueberryhub92@users.noreply.github.com>
2024-10-27 12:54:37 +0100

    add api + lambda + auth + graphql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
-import { GradientBackgroundCon } from "@/components/QuoteGenerator/QuoteGeneratorElements";
 import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+
+// Components
+import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, GenerateQuoteButton, GenerateQuoteButtonText, GradientBackgroundCon, QuoteGenerationSubTitle, QuoteGeneratorCon, QuoteGeneratorInnerCon, QuoteGeneratorTitle, RedSpan } from "@/components/QuoteGenerator/QuoteGeneratorElements";
+
+// Assets
+import Clouds1 from "../assets/Clouds1.png";
+import Clouds2 from "../assets/Clouds2.png";
+import { useState } from "react";
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<number | null>(0);
   return (
     <>
       <Head>
@@ -12,6 +22,55 @@ export default function Home() {
       </Head>
       {/*Background*/}
       <GradientBackgroundCon>
+
+      {/*Quote Generator Modal Pop-Up*/}
+      {/* <QuoteGeneratorModal
+        // numberOfQuotes={numberOfQuotes}
+        // setNumberOfQuotes={setNumberOfQuotes}
+      /> */}
+
+      {/*Quote Generator*/}
+      <QuoteGeneratorCon>
+        <QuoteGeneratorInnerCon>
+          <QuoteGeneratorTitle>
+            Daily Inspiration Generator
+          </QuoteGeneratorTitle>
+
+          <QuoteGenerationSubTitle>
+            Looking for a splash of inspiration? Generate a quote card with a random inspirational quote
+            provided by <FooterLink href="https://zenquotes.io/" target="_blank" rel="noopener noreferrer">ZenQuotes API</FooterLink>.
+          </QuoteGenerationSubTitle>
+
+          <GenerateQuoteButton>
+            <GenerateQuoteButtonText onClick={() => setNumberOfQuotes(prev => prev + 1)}>
+              Generate Quote
+            </GenerateQuoteButtonText>
+          </GenerateQuoteButton>
+        </QuoteGeneratorInnerCon>
+      </QuoteGeneratorCon>
+
+      {/*Background Images*/}
+      <BackgroundImage1
+        src={Clouds1}
+        height="300"
+        alt="cloudybackground1"
+      />
+
+      <BackgroundImage2
+        src={Clouds2}
+        height="300"
+        alt="cloudybackground2"
+      />
+
+      {/*Footer*/}
+      <FooterCon>
+        <>
+          Quotes Generated: {numberOfQuotes}
+          <br />
+          Developed with ❤️ by <FooterLink href="https://github.com/blueberryhub92" target="_blank" rel="noopener noreferrer">blueberryhub92</FooterLink>
+        </>
+      </FooterCon>
+
       </GradientBackgroundCon>
     </>
   );
